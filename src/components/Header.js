b82b5bb88586b33a5df83fdba56af8d7cb07bcd7
mohamed.ehab <mohamed.ehab@mytigate.com>
2025-01-26 15:36:52 +0200

    Setup Tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
+import { Glance } from "../utils/svgs"
+
 const Header = () => {
     return (
-        <p>Header</p>
+        <header className="flex p-5 shadow-xs border-b border-gray-200 fixed w-full">
+            <Glance/>
+        </header>
     )
 }
 
